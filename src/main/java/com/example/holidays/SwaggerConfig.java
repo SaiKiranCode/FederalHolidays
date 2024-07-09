39c8d4d65b319ba169e273135e6a1d1be303b613
Sai Kiran Beeravolu <61832514+SaiKiranCode@users.noreply.github.com>
2024-07-10 01:10:47 +0530

    swagger code commented

--- src/main/java/com/example/holidays/SwaggerConfig.java
+++ src/main/java/com/example/holidays/SwaggerConfig.java
@@ -1,12 +1,16 @@
 //package com.example.holidays;
 //
+//import com.google.common.base.Predicate;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.RequestHandler;
 //import springfox.documentation.builders.PathSelectors;
 //import springfox.documentation.builders.RequestHandlerSelectors;
 //import springfox.documentation.spi.DocumentationType;
 //import springfox.documentation.spring.web.plugins.Docket;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//import java.util.Arrays;
 //
 //@Configuration
 //@EnableSwagger2
@@ -14,10 +18,7 @@
 //
 //    @Bean
 //    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
+//        return  new Docket(DocumentationType.SWAGGER_2).select()
+//		.apis((Predicate<RequestHandler>) RequestHandlerSelectors.basePackage("com.example.holidays.controller")).paths((Predicate<String>) PathSelectors.regex("/.*")).build();
+//}
 //}
